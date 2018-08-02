ea8cd7de6a627192dc5bb4bd0b35e8961835e829
TS Planning Engineer <ym5@ualberta.ca>
2018-08-02 11:15:12 -0600

    fix reading data issue. 11,234.00 was not recognizable previously

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -12,6 +12,9 @@ var popEmp;
 var accessibilityResult;
 var travelTypeDict = {};
 var q = d3.queue();
+var check = false;
+var largestIndividualArray = [];
+var selectZone = '101'; //default
 q.defer(d3.csv,Distance_mf2)
     .defer(d3.csv,SOV_AUTO_Time_AM_Cr_mf1)
     .defer(d3.csv,Transit_Total_Time_AM)
@@ -28,6 +31,7 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
     travelJson = travelTypeDict[travelType];//default
     popEmp = buildMatrixLookup2(pop_emp_pse_hs);
     require([
+      "esri/geometry/Polyline",
       "esri/geometry/Extent",
       "dojo/dom-construct",
       "esri/tasks/query",
@@ -41,7 +45,7 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
         "esri/InfoTemplate", "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
         "esri/renderers/ClassBreaksRenderer",
         "esri/Color", "dojo/dom-style", "dojo/domReady!"
-    ], function(
+    ], function(Polyline,
       Extent,domConstruct,
       Query,Popup, PopupTemplate,domClass,BasemapToggle,Legend,
         GeoJsonLayer,Map, FeatureLayer,
@@ -51,36 +55,27 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
     ) {
         var connections = [];
         
-        var popup = new Popup({
+        var popup = new Popup({  
           fillSymbol:
             new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
               new Color([255, 0, 0]), 2)
         }, domConstruct.create("div"));
-
-        domClass.add(popup.domNode, "myTheme");
-
+  
         map = new Map("map", {
             basemap: "dark-gray-vector",
             center: [-113.4909, 53.5444],
             zoom: 9,
             minZoom:6,
-               infoWindow: popup,
+            infoWindow: popup,
             slider: false
         });
         map.setInfoWindowOnClick(true);
-        
-        map.on("click", function (event) {
-          var query = new Query();
-          query.geometry = pointToExtent(map, event.mapPoint, 10);
-          var deferred = featureLayer.selectFeatures(query,
-            FeatureLayer.SELECTION_NEW);
-          map.infoWindow.setFeatures([deferred]);
-          map.infoWindow.show(event.mapPoint);
-        });
+
+
 
         var toggle = new BasemapToggle({
            map: map,
-           basemap: "topo"
+           basemap: "streets"
          }, "viewDiv");
          
          toggle.startup();
@@ -93,38 +88,72 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
             outFields: ["*"],
             infoTemplate: template
         });
+        featureLayer.on('click',function(evt){
+          
+          var graphic = evt.graphic;
+          selectZone = graphic.attributes.TAZ_New;
+          
+        })
+    
         var legendLayers = [];
-        legendLayers.push({ layer: featureLayer, title: 'Edmonton Layer' });
+        legendLayers.push({ layer: featureLayer, title: 'Legend' });
         map.on('load',function(){
             map.addLayer(featureLayer);
-
             redrawLayer(ClassBreaksRenderer,accessibilityResult);
         });
 
         function redrawLayer(ClassBreaksRenderer,accessibilityResult){
 
             $('.legendClass').remove();
-            var accessibilityResultArray = Object.values(accessibilityResult);
-            var min = accessibilityResultArray.sort((prev, next) => prev - next)[0];
-            var max =accessibilityResultArray.sort((prev, next) => next - prev)[1];
-            var chunk = 8;
-            var chunksize =(max-min)/chunk;
+
+            // console.log(accessibilityResultArray)
+            
+            
+            // var min = accessibilityResultArray.sort((prev, next) => prev - next)[0];
+            // var max =accessibilityResultArray.sort((prev, next) => next - prev)[1];
+            var sort = [];
+            if(check === true){
+              var largestIndividualResultArray = Object.values(largestIndividualArray);
+              sort = largestIndividualResultArray.sort((prev,next)=>prev-next); //from smallest to largest
+              
+            }
+            else{
+              var accessibilityResultArray = Object.values(accessibilityResult);
+              sort = accessibilityResultArray.sort((prev,next)=>prev-next); //from smallest to largest
+            }
+
+            var chunkZones = 74;        
+            sort = sort.map(function (x) { 
+              return parseInt(x, 10); 
+            });  
             var symbol = new SimpleFillSymbol();
-
             var renderer = new ClassBreaksRenderer(symbol, function(feature){
-                return accessibilityResult[feature.attributes.TAZ_New];
+          
+              var r = accessibilityResult[feature.attributes.TAZ_New];
+  
+              return accessibilityResult[feature.attributes.TAZ_New];
             });
-
-            renderer.addBreak(0, chunksize, new SimpleFillSymbol().setColor(new Color([237,248,251, 1.0])));
-            renderer.addBreak(chunksize, 2*chunksize, new SimpleFillSymbol().setColor(new Color([237,248,251, 1.0])));
-            renderer.addBreak(2*chunksize, 3*chunksize, new SimpleFillSymbol().setColor(new Color([204,236,230,1.0])));
-            renderer.addBreak(3*chunksize, 4*chunksize, new SimpleFillSymbol().setColor(new Color([153,216,201, 1.0])));
-            renderer.addBreak(4*chunksize, 5*chunksize, new SimpleFillSymbol().setColor(new Color([102,194,164, 1.0])));
-            renderer.addBreak(5*chunksize, 6*chunksize, new SimpleFillSymbol().setColor(new Color([65,174,118, 1.0])));
-            renderer.addBreak(6*chunksize, 7*chunksize, new SimpleFillSymbol().setColor(new Color([35,139,69, 1.0])));
-            renderer.addBreak(7*chunksize, Infinity, new SimpleFillSymbol().setColor(new Color([0,88,36, 1.0])));
+            // renderer.addBreak(-Infinity,0, new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([153, 153, 153,0.90])));
+            renderer.addBreak(0, sort[chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([255, 255, 255,0.90])));
+            renderer.addBreak(sort[chunkZones], sort[2*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([	249, 238, 237,0.90])));
+            renderer.addBreak(sort[2*chunkZones], sort[3*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([243, 224, 219,0.90])));
+            renderer.addBreak(sort[3*chunkZones], sort[4*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([237, 214, 202,0.90])));
+            renderer.addBreak( sort[4*chunkZones], sort[5*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([225, 200, 170,0.90])));
+            renderer.addBreak( sort[5*chunkZones],  sort[6*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([213, 196, 141,0.90])));
+            renderer.addBreak( sort[6*chunkZones],  sort[7*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([207, 197, 127,0.90])));
+            renderer.addBreak(sort[7*chunkZones], sort[8*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([201, 199, 113,0.90])));
+            renderer.addBreak(sort[8*chunkZones], sort[9*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([185, 195, 101,0.90])));
+            renderer.addBreak(sort[9*chunkZones], sort[10*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([168, 189, 88,0.90])));
+            renderer.addBreak(sort[10*chunkZones], sort[11*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([149, 183, 77,0.90])));
+            renderer.addBreak(sort[11*chunkZones], sort[12*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([129, 177, 66,0.90])));
+            renderer.addBreak(sort[12*chunkZones], sort[14*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([109, 171, 55,0.90])));
+            renderer.addBreak(sort[14*chunkZones], sort[16*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([87, 165, 45,0.90])));
+            renderer.addBreak(sort[16*chunkZones], sort[18*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([	66, 159, 36,0.90])));
+            renderer.addBreak(sort[18*chunkZones], sort[20*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([44, 153, 27,0.90])));  
+            renderer.addBreak(sort[20*chunkZones], sort[22*chunkZones], new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([	37, 121, 24,0.90])));
+            renderer.addBreak(sort[22*chunkZones], sort[sort.length-1]+1, new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([0,0,0,0.1]),1)).setColor(new Color([11, 106, 18,0.90])));
             featureLayer.setRenderer(renderer);
-
+      
             featureLayer.redraw();
             
             var string = 'dynamicLegend'+ Math.random().toString(36).substring(7);
@@ -153,22 +182,43 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
 
         $("#travelMethod").change(function(){
             travelJson = travelTypeDict[$(this).val()];
-            accessibilityResult = accessibilityCalculation(travelJson,jobType);
+    
+            if(check === true){
+              largestIndividualArray = findRangeForIndividualCalcultion(travelJson,jobType);
+              accessibilityResult = individualCaculation(travelJson,jobType,selectZone);
+
+            }
+            else{
+              accessibilityResult = accessibilityCalculation(travelJson,jobType);
+            }
             redrawLayer(ClassBreaksRenderer,accessibilityResult);
+
         });
         $('#jobType').change(function(){
           jobType = $(this).val();
-          accessibilityResult = accessibilityCalculation(travelJson,jobType);
-          redrawLayer(ClassBreaksRenderer,accessibilityResult);
+          if(check === true){
+            largestIndividualArray = findRangeForIndividualCalcultion(travelJson,jobType);
+            accessibilityResult = individualCaculation(travelJson,jobType,selectZone);
+
+          }
+          else{
+            accessibilityResult = accessibilityCalculation(travelJson,jobType);
+          }
+          redrawLayer(ClassBreaksRenderer,accessibilityResult)
         });
         $("#interact").click(function(e, parameters) {
             
             if($("#interact").is(':checked')){
+                check = true;
                 connections.push(dojo.connect(map.getLayer(map.graphicsLayerIds[0]), 'onClick', MouseClickhighlightGraphic));
                 connections.push(dojo.connect(map.getLayer(map.graphicsLayerIds[0]), 'onMouseOver', MouseOverhighlightGraphic));
+                largestIndividualArray = findRangeForIndividualCalcultion(travelJson,jobType);
+                accessibilityResult = individualCaculation(travelJson,jobType,selectZone);
+                redrawLayer(ClassBreaksRenderer,accessibilityResult);
 
             }
             else{
+              check = false;
               dojo.forEach(connections,dojo.disconnect);
               accessibilityResult = accessibilityCalculation(travelJson,jobType);
               redrawLayer(ClassBreaksRenderer,accessibilityResult);
@@ -179,6 +229,12 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
             var graphic = evt.graphic;
             selectZone = graphic.attributes.TAZ_New;
             accessibilityResult = individualCaculation(travelJson,jobType,selectZone);
+            var query = new Query();
+            query.geometry = pointToExtent(map, event.mapPoint, 10);
+            var deferred = featureLayer.selectFeatures(query,
+              FeatureLayer.SELECTION_NEW);
+            map.infoWindow.setFeatures([deferred]);
+            map.infoWindow.show(event.mapPoint);
             redrawLayer(ClassBreaksRenderer,accessibilityResult);
         };
         
@@ -187,26 +243,14 @@ function brushMap(error,distance_mf2,sov_auto_time,transit_total_time,walk_time,
           hoverZone = graphic.attributes.TAZ_New;
           var access = accessibilityResult[hoverZone];
 
-
-          //console.log(graphic._graphicsLayer);
-          // assuming the graphic has a SimpleLineSymbol...
-          //symbol.setColor("red");
-          //graphic.setSymbol(symbol);
-
           map.infoWindow.setTitle("<b>Zone Number: </b>"+hoverZone);
-          map.infoWindow.setContent("<b><font size=\"3\"> Value:</font> </b>"+ "<font size=\"4\">"+access.toFixed(2)+"</font>");
+          if(typeof(access)!=='undefined'){
+            map.infoWindow.setContent("<b><font size=\"3\"> Value:</font> </b>"+ "<font size=\"4\">"+access.toFixed(2)+"</font>");
+          }
+          else{
+            map.infoWindow.setContent("<b><font size=\"3\"> Value:</font> </b>"+ "<font size=\"4\">"+'undefined'+"</font>");
+          }
           map.infoWindow.show(evt.screenPoint,map.getInfoWindowAnchor(evt.screenPoint));
-
-          // var highlightSymbol = new SimpleFillSymbol(
-          //     SimpleFillSymbol.STYLE_SOLID,
-          //     new SimpleLineSymbol(
-          //         SimpleLineSymbol.STYLE_SOLID,
-          //         new Color([255,0,0,0.5]), 3
-          //     ),
-          //     new Color([255,0,0,0.5])
-          // );
-          // var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
-          // map.graphics.add(highlightGraphic)
       };
     });
 
@@ -236,12 +280,18 @@ function buildMatrixLookup2(arr) {
     var zoneName = Object.keys(arr[0])[1];
 
     arr.forEach(row => {
+        for(var k in row){
+          
+          row[k] = row[k].replace(',','');
+          row[k] = row[k].replace('-','');
+          row[k] = row[k].replace(' ','');
+        }
         lookup[row[zoneName]]= row;
+        
     });
     return lookup;
 }
 function accessibilityCalculation(transitMatrix,jobType){
-
     var accessibilityArray ={};
     for (var zone in transitMatrix){
         var result = 0;
@@ -260,16 +310,27 @@ function accessibilityCalculation(transitMatrix,jobType){
 }
 function individualCaculation(transitMatrix,jobType,selectedZone){
     var accessibilityArray = {};
+    var enr = parseFloat(popEmp[selectedZone][jobType]);
     for(var destZone in transitMatrix[selectedZone]){
-      if(typeof(popEmp[destZone])!=='undefined'){
-        var num = transitMatrix[selectZone][destZone];
-        var enr = Number(popEmp[selectZone][jobType]);
+      if(typeof(popEmp[selectedZone])!=='undefined'){
+        var num = transitMatrix[selectedZone][destZone];
         if (Number(num)!==0 && isNaN(enr) === false){
             accessibilityArray[destZone] =  enr/Math.pow(num,1.285);
         }
       }  
     }
     return accessibilityArray;
-  
-  
+}
+function findRangeForIndividualCalcultion(transitMatrix,jobType){
+
+  var TAZ = 0;
+  var max = 0;
+  for(var k in popEmp){
+      if(Number(popEmp[k][jobType])>max){
+          TAZ = popEmp[k]['New Zone'];
+          max = Number(popEmp[k][jobType]);
+      }
+  }
+  var largestAccessibilityArray = individualCaculation(transitMatrix,jobType,TAZ);
+  return largestAccessibilityArray;
 }
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,31 @@ var express = require('express');
 var router = express.Router();
 var conString = "";
 var obj = "1";
-/* GET home page. */
+var fs = require('fs');
+var RSVP = require('RSVP');
+
+// 
+// console.log('1');
+// var promise = new RSVP.Promise(function(fulfill, reject) {
+//   var file = fs.readFile('./public/data/Distance_mf2.csv','UTF-8',function(error,csv){
+// 
+//     fulfill(csv)
+// 
+//   })
+// 
+//   console.log('123')
+// });
+// 
+// promise.then(function(c) {
+// 
+//   router.get('/', function(req, res, next) {
+//       res.render('index', { title: 'Job Accessibility'});
+//   });
+// });
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Job Accessibility'});
+    res.render('index', { title: 'Accessibility-Gravity Model'});
 });
-
 
 
 
